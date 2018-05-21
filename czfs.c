0918a8678a6e06f7920782890d8d27dc2dd498c5
Philip Barr <17pb10@caslab.queensu.ca>
2018-05-21 13:08:27 -0400

    avance

--- czfs.c
+++ czfs.c
@@ -3,104 +3,7 @@
 #include <unistd.h>
 #include <string.h>
 #include <windows.h>
-
-
-
-typedef struct bloque bloque;
-struct bloque {
-	unsigned char block_size[1024];	
-};
-
-typedef int puntero;
-
-
-typedef struct czFILE czFILE;
-struct czFILE {
-	bloque arreglo_bloques[65536];
-};
-
-typedef struct entrada_directorio entrada_directorio;
-struct entrada_directorio{
-	unsigned char valid;
-	unsigned char nombre_archivo[11];
-	puntero indice;
-};
-
-typedef struct directorio directorio;
-struct directorio {
-	entrada_directorio entradas_directorio[64];
-};
-
-typedef struct bitmap bitmap;
-struct bitmap{
-	unsigned char bitmap[1024];	
-};
-
-typedef struct indice indice;
-struct indice {
-	unsigned char size[4];
-	unsigned char timestamp_creacion[4];
-	unsigned char timestamp_modificacion[4];
-	puntero punteros_directos[1008];
-	puntero puntero_indirecto;
-};
-
-typedef struct datos datos;
-struct datos {
-	unsigned char data[1024];
-};
-
-typedef struct indirecto indirecto;
-struct indirecto {
-	puntero punteros_directos[256];
-};
-
-
-czFILE* cz_open(char* filename, char mode){
-	FILE *ptr;
-	const char read = 'r';
-
-	if (read == mode) {
-   		ptr = fopen(filename,"rb"); // r for read, b for binary		}
-	}else{
-	 printf("%s\n", "doesnt match");}
-
-	printf("%c\n", read);
-	printf("%c\n", mode);
-}
-
-int cz_exists(char* filename){
-	return 0;
-}
-
-int cz_read(czFILE* file_desc, void* buffer, int nbytes){
-	return 0;
-}
-
-int cz_write(czFILE* file_desc, void* buffer, int nbytes){
-	return 0;
-}
-
-int cz_close(czFILE* file_desc){
-	return 0;
-}
-
-int cz_mv(char* orig, char *dest){
-	return 0;
-}
-
-int cz_cp(char* orig, char* dest){
-	return 0;
-}
-
-int cz_rm(char* filename){
-	return 0;
-}
-
-void cz_ls(){
-
-}
-
+#include "funciones.c"
 
 int main(int argc, char *argv[]) {
 	printf("size puntero: %lu\n", sizeof(puntero));
@@ -109,18 +12,13 @@ int main(int argc, char *argv[]) {
 	printf("size czfile: %lu\n", sizeof(czFILE));
 	printf("size bloque: %lu\n", sizeof(bloque));
 	printf("size bitmap: %lu\n", sizeof(bitmap));
+	printf("size indice: %lu\n", sizeof(indice));
 	printf("size datos: %lu\n", sizeof(datos));
 	printf("size indirecto: %lu\n", sizeof(indirecto));
 
-	indice *prueba = (indice*) calloc(1, sizeof(indice));
-	int *num = (int*) calloc(1, sizeof(int));
-	printf("%d, %d --\n", *prueba->size, *num);
 	char a;
+	cz_open("", 'r');
 
 	scanf("%c", &a);
-	printf("algo: %c\n", a);	
-	//printf("algo: %s\n", a=='rb');
 	scanf("%c", &a);
-	char nombre[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
-	cz_open(nombre, 'r');
 }
---
+++ funciones.c
@@ -0,0 +1,138 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <windows.h>
+#include "estructuras.c"
+
+
+
+directorio *generar_directorio(char* filename){
+	directorio *directorio_generado = calloc(1, sizeof(directorio));
+	FILE * fp;
+	fp=fopen(filename, "rb");
+    fread(directorio_generado, sizeof(*directorio_generado), 1, fp);
+    return directorio_generado;
+}
+
+int cz_exists(char* filename){
+	directorio *directorio_disco = calloc(1, sizeof(directorio));
+	directorio_disco = generar_directorio("simdiskformat.bin");
+
+	int archivo_existe = -1;
+	for (int i=0; i < sizeof(directorio)/sizeof(entrada_directorio); i++){
+		if ((strcmp(directorio_disco->entradas_directorio[i].nombre_archivo, filename) == 0) & (directorio_disco->entradas_directorio[i].valid != 0)){
+			archivo_existe = 1;
+		}
+	}
+	return archivo_existe;
+}
+
+int cz_read(czFILE* file_desc, void* buffer, int nbytes){
+	return 0;
+}
+
+int cz_write(czFILE* file_desc, void* buffer, int nbytes){
+	return 0;
+}
+
+int cz_close(czFILE* file_desc){
+	return 0;
+}
+
+int cz_mv(char* orig, char *dest){
+	return 0;
+}
+
+int cz_cp(char* orig, char* dest){
+	return 0;
+}
+
+int cz_rm(char* filename){
+	return 0;
+}
+
+void cz_ls(){
+	directorio *directorio_disco = calloc(1, sizeof(directorio));
+	directorio_disco = generar_directorio("simdiskformat.bin");
+
+	for (int i=0; i < sizeof(directorio)/sizeof(entrada_directorio); i++){
+		if (directorio_disco->entradas_directorio[i].valid != 0){
+			printf("nombre archivo %d: %s\n", i, directorio_disco->entradas_directorio[i].nombre_archivo);
+		}
+	}
+}
+
+void showbits(unsigned char x){
+    int i; 
+    for(i=(sizeof(x)*8)-1; i>=0; i--)
+            (x&(1u<<i))?putchar('1'):putchar('0');
+    
+    printf("\n");
+}
+
+unsigned char modifybitto1(unsigned char *byte, int n_bit)
+{
+    *byte = *byte|(1u<<n_bit);
+    return *byte;
+}
+
+czFILE* cz_open(char* filename, char mode){
+	const char read = 'r';
+	indice *indice_archivo = calloc(1, sizeof(indice));
+
+	directorio *directorio_disco = calloc(1, sizeof(directorio));
+	directorio_disco = generar_directorio("simdiskformat.bin");
+
+
+	if (read == mode) {
+   		//ptr = fopen(filename,"rb"); // r for read, b for binary		}
+
+		int n_archivo = -1;
+		//Encontramos el archivo dentro del directorio
+		for (int i=0; i < sizeof(directorio)/sizeof(entrada_directorio); i++){
+			if (strcmp(directorio_disco->entradas_directorio[i].nombre_archivo, filename) == 0){
+				//agregar if validez
+				n_archivo = i;
+			}
+		}
+		if (n_archivo == -1){
+			return NULL;
+		}
+
+		//abrir el archivo indice
+		FILE * fp;
+		fp=fopen("simdiskformat.bin", "rb");
+	    fseek(fp, directorio_disco->entradas_directorio[n_archivo].ubicacion_indice, SEEK_SET);
+	    fread(indice_archivo, sizeof(indice), 1, fp);
+	    datos *bloque_datos = calloc(1, sizeof(datos));
+
+	    //copiar la información del archivo a ret
+		czFILE *ret = calloc(1, sizeof(czFILE));
+		ret->mode = mode;
+		//+2 para probar aunque el disco esté vacio
+		ret->size = indice_archivo->size + 2;
+		ret->content = calloc(ret->size, sizeof(char));
+		for(int n_bloque_datos=0; n_bloque_datos < 252; n_bloque_datos++){
+			fseek(fp, indice_archivo->punteros_directos[n_bloque_datos], SEEK_SET);
+		    fread(bloque_datos->data, sizeof(datos), 1, fp);
+			for (int n_linea_bloque=0; n_linea_bloque < 1024; n_linea_bloque++){
+				if (n_bloque_datos*1024 + n_linea_bloque < indice_archivo->size + 2){
+					ret->content[n_bloque_datos*1024 + n_linea_bloque] = bloque_datos->data[n_linea_bloque];
+				}
+			}
+		}
+
+		//mostrar información
+		for(int byte_n=0; byte_n < indice_archivo->size + 2; byte_n++){
+			showbits(ret->content[byte_n]);
+		}
+		return ret;
+	}else{
+
+	}
+	printf("%c\n", read);
+	printf("%c\n", mode);
+	czFILE *hola = calloc(1, sizeof(czFILE));
+	return hola;
+}
